4a11e1008d1d75522f73dddbcc001310197e8beb
Suma H <sumah2004@gmail.com>
2026-05-01 17:20:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,6 @@ app.use("/api/tasks", require("./routes/tasks"));
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
